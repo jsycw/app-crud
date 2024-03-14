4e86745a4623fd46c044bb5ea4173d28aaafd9f1
Anastashia Ellena <a.ellena1898@gmail.com>
2024-03-14 21:46:09 +0700

    ellena commit

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ Route::post('/product', [ProductController::class, 'store'])->name('product.stor
 Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
 Route::put('/product/{product}/update', [ProductController::class, 'update'])->name('product.update');
 Route::delete('/product/{product}/delete', [ProductController::class, 'delete'])->name('product.delete');
+
+
+// halo halo bandung ibu kota
